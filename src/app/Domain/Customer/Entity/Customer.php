f07bc92cd6d4d868ae8c64dbff86b1dd79fdb14e
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-08-23 19:33:14 +0100

    Add composer.lock file and Customer entity

--- src/app/Domain/Customer/Entity/Customer.php
+++ src/app/Domain/Customer/Entity/Customer.php
@@ -1,15 +1,51 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: joesweeny
- * Date: 23/08/18
- * Time: 19:02
- */
 
 namespace Wranx\Domain\Customer\Entity;
 
-
 class Customer
 {
+    /**
+     * @var string
+     */
+    private $title;
+    /**
+     * @var string
+     */
+    private $firstName;
+    /**
+     * @var string
+     */
+    private $lastName;
+    /**
+     * @var string
+     */
+    private $address;
 
+    public function __construct(string $title, string $firstName, string $lastName, string $address)
+    {
+        $this->title = $title;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->address = $address;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
 }
