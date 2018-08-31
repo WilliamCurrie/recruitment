31ac7826cc9a71b14972db445352c3209f99ddd0
unknown <adambicknell86@gmail.com>
2018-08-31 23:06:06 +0100

    Adding gulpfile.js.

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+var less = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+var embedlr = require('gulp-embedlr');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var image = require('gulp-image');
+
+gulp.task('image', function () {
+  gulp.src(['./resources/images/**/*'])
+    .pipe(image())
+    .pipe(gulp.dest('./public/img'));
+});
+
+gulp.task('scripts', function() {
+    gulp.src(['./resources/scripts/vendor/jquery.js', './resources/scripts/wranx.js'])
+        .pipe(browserify())
+        .pipe(concat('wranx.js'))
+        .pipe(gulp.dest('./public/js'))
+        .pipe(uglify())
+        .pipe(rename('wranx.min.js'))
+        .pipe(gulp.dest('./public/js'))
+})
+
+gulp.task('styles', function() {
+    gulp.src(['./resources/styles/wranx.scss'])
+        .pipe(less())
+        .pipe(rename("wranx.css"))
+        .pipe(gulp.dest('./public/css'))
+        .pipe(minifyCSS())
+        .pipe(rename("wranx.min.css"))
+        .pipe(gulp.dest('./public/css'))
+})
+
+
+gulp.task('default', function() {
+    gulp.run('scripts');
+    gulp.run('styles');
+})
