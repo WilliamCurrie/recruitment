ebbf49f3455b268a52836d27842f7115ec9bd0e7
unknown <adambicknell86@gmail.com>
2018-09-01 17:02:42 +0100

    Added a little bit of style.

--- resources/scripts/wranx.js
+++ resources/scripts/wranx.js
@@ -21,7 +21,18 @@ $(function(){
         dataType: 'json', // what type of data do we expect back from the server
         encode: true
       }).done(function(data) {
-        console.log(data);
+        var $innerWrapH2 = $('.inner-wrapper h2');
+        $innerWrapH2.text(data.message);
+        if(data.result == 'success') {
+          $innerWrapH2.css('color', 'green');
+        } else {
+          $innerWrapH2.css('color', 'red');
+        }
+
+        setTimeout(function() {
+            $innerWrapH2.text('Add Customer').css('color', '#fff');
+        }, 5000)
+
       });
       event.preventDefault();
     });
